18eec3f81a7df0c0f3e777dfbc986b892e284675
Usuario <Usuario@CPD02-M03>
2024-07-30 12:03:44 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsAppI/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsAppI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.btnsomar = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnfechar = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primeiro número:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 15.75F);
+            this.label2.Location = new System.Drawing.Point(45, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Segundo número:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 15.75F);
+            this.label3.Location = new System.Drawing.Point(45, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado:";
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Font = new System.Drawing.Font("Book Antiqua", 25.75F);
+            this.lblresult.Location = new System.Drawing.Point(333, 204);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(36, 41);
+            this.lblresult.TabIndex = 3;
+            this.lblresult.Text = "0";
+            // 
+            // btnsomar
+            // 
+            this.btnsomar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsomar.Location = new System.Drawing.Point(591, 52);
+            this.btnsomar.Name = "btnsomar";
+            this.btnsomar.Size = new System.Drawing.Size(151, 60);
+            this.btnsomar.TabIndex = 4;
+            this.btnsomar.Text = "Somar";
+            this.btnsomar.UseVisualStyleBackColor = true;
+            this.btnsomar.Click += new System.EventHandler(this.btnsomar_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F);
+            this.btnlimpar.Location = new System.Drawing.Point(591, 158);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(151, 60);
+            this.btnlimpar.TabIndex = 5;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnfechar
+            // 
+            this.btnfechar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F);
+            this.btnfechar.Location = new System.Drawing.Point(591, 264);
+            this.btnfechar.Name = "btnfechar";
+            this.btnfechar.Size = new System.Drawing.Size(151, 60);
+            this.btnfechar.TabIndex = 6;
+            this.btnfechar.Text = "Fechar";
+            this.btnfechar.UseVisualStyleBackColor = true;
+            this.btnfechar.Click += new System.EventHandler(this.btnfechar_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(227, 124);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(259, 20);
+            this.txt2.TabIndex = 7;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(227, 68);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(259, 20);
+            this.txt1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.btnfechar);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnsomar);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Somas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.Button btnsomar;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnfechar;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
---
+++ WindowsFormsAppI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsomar_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(txt1.Text);
+            int y = int.Parse(txt2.Text);
+
+            int z = x + y;
+
+            lblresult.Text = z.ToString();
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txt1.Clear();
+            txt2.Clear();
+
+            lblresult.Text = "0";
+
+            txt1.Focus();
+        }
+
+        private void btnfechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
